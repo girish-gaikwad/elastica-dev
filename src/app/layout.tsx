0f96190a400f21f4b2a4da8dd6f6525ba4b44b47
girish <girishgaikwad2055@gmail.com>
2025-03-04 18:01:09 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon"  href="/elastica.png" />
+        {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+
+      </head>
       <body className="font-sans">
         <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
